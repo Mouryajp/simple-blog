d3ce30bb2b1af2ce31bcdb1f6b3f1e92be183de1
Mourya J P <128205789+Mouryajp@users.noreply.github.com>
2024-05-31 22:35:37 +0530

    page.js

--- src/app/blogs/page.js
+++ src/app/blogs/page.js
@@ -4,7 +4,7 @@ async function fetchListOfBlogs() {
   
   try {
     
-    const apiResponse = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/get-blogs`, {
+    const apiResponse = await fetch("simple-blog-brown.vercel.app/api/get-blogs", {
     
       method: "GET",
       cache: "no-store",
