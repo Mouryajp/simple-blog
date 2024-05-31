af10fddc5e039ddf8eb808671ef6857cd86fb54b
Mourya J P <128205789+Mouryajp@users.noreply.github.com>
2024-05-31 22:05:53 +0530

    page.js

--- src/app/blogs/page.js
+++ src/app/blogs/page.js
@@ -3,13 +3,9 @@ import BlogOverview from "@/components/blog-overview";
 async function fetchListOfBlogs() {
   
   try {
-<<<<<<< HEAD
     
     const apiResponse = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/get-blogs`, {
     
-=======
-      const apiResponse = await fetch("http://localhost:3000/api/get-blogs", {
->>>>>>> 0b5072b25f67373c60a416aefd4681463f85cda1
       method: "GET",
       cache: "no-store",
     });
